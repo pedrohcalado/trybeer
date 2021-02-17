14cff9b86f53c02148032f7821dd33c2f0c883a3
pedrohcalado <pedrocalado22@gmail.com>
2021-02-17 17:29:13 -0300

    Tentativas de chat

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const http = require('http').createServer(app);
+const dateFormat = require('dateformat');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
 const errorMiddleware = require('./middlewares/error');
 const userController = require('./controllers/userController');
 const productsController = require('./controllers/productsController.js');
@@ -22,8 +31,7 @@ app.use('/checkout', checkoutController);
 app.use('/products', productsController);
 app.use(errorMiddleware);
 
-
 app.use('/images/', express.static(path.join(__dirname, '/images')));
 
 const PORT = 3001;
-app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
+http.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
